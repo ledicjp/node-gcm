4b0b5e3fad654f6a4289732562f1e53a96f14092
ledicjp <ledicjp@gmail.com>
2016-04-25 19:11:19 +0200

    Update notification.js

--- examples/notification.js
+++ examples/notification.js
@@ -9,9 +9,9 @@ message.addNotification('body', 'World');
 
 
 //Add your mobile device registration tokens here
-var regTokens = ['ecG3ps_bNBk:xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxXl7TDJkW'];
+var regTokens = ['cy9G3Af59ko:APA91bHiIaDN7KfcN-JdrIhdQchYIiKmRZvfjWORF4X0IboN6pYD6ITJlLf_6djxbcINP8zaqk5W0pQFl7J4hY6o5x2I3qr4mD1iv-ZTK8_nwOLbnYWxj95TQ5fGhIy4ASnHVoEgCBVV'];
 //Replace your developer API key with GCM enabled here
-var sender = new gcm.Sender('AIza*******************5O6FM');
+var sender = new gcm.Sender('AIzaSyDW5PnsQP-brJ0vWEs7wtgYUAkMX9VhIKY');
 
 sender.send(message, regTokens, function (err, response) {
     if(err) {
